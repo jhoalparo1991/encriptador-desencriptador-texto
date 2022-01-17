78978f5c2655ff8e9788f6f7edc3d0d351ab7146
jhonatan padilla <jhoalparo1991@gmail.com>
2022-01-17 17:34:38 -0500

    Se implmentó el boton de copiar texto en el clipboard

---
+++ js/copiar.js
@@ -0,0 +1,9 @@
+let textoCifrado = document.querySelector("#texto_cifrado");
+
+let btncopiar = document.querySelector("#copiar");
+
+btncopiar.addEventListener("click", (e) => {
+  e.preventDefault();
+  textoCifrado.select();
+  navigator.clipboard.writeText(textoCifrado.value)
+});
