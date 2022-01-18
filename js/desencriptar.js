51394f5ac32746a8ad383697363d901ea900fbd0
jhonatan padilla <jhoalparo1991@gmail.com>
2022-01-18 08:43:25 -0500

    se agrego validacion en el archivo de encriptar y desencriptar para cambiar numeros por espacios vacios

--- js/desencriptar.js
+++ js/desencriptar.js
@@ -21,7 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
       .replace(/imes/g, "i")
       .replace(/ai/g, "a")
       .replace(/ober/g, "o")
-      .replace(/ufat/g, "u");
+      .replace(/ufat/g, "u")
+      .replace(/0/g, "")
+      .replace(/1/g, "")
+      .replace(/2/g, "")
+      .replace(/3/g, "")
+      .replace(/4/g, "")
+      .replace(/5/g, "")
+      .replace(/6/g, "")
+      .replace(/7/g, "")
+      .replace(/8/g, "")
+      .replace(/9/g, "");
     console.log(nuevoTexto.toLowerCase());
     textoCifrado.value = nuevoTexto.toLowerCase();
     //  Clear textarea
@@ -39,6 +49,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     return false;
   }
-
-
 });
--- js/encriptar.js
+++ js/encriptar.js
@@ -24,7 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+        .replace(/u/g, "ufat")
+        .replace(/0/g, "")
+        .replace(/1/g, "")
+        .replace(/2/g, "")
+        .replace(/3/g, "")
+        .replace(/4/g, "")
+        .replace(/5/g, "")
+        .replace(/6/g, "")
+        .replace(/7/g, "")
+        .replace(/8/g, "")
+        .replace(/9/g, "");
         nuevoTexto.push(cifrado);
     });
 
