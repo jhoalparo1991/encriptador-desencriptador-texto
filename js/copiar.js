040fb311929919f14c25c12dff3624d1a02d7b4a
Jhonatan Padilla <jhoalparo1991@gmail.com>
2022-01-17 20:46:51 -0500

    Se hicieron mejoras en el css y en el metodo copiar

--- js/copiar.js
+++ js/copiar.js
@@ -1,9 +1,14 @@
 let textoCifrado = document.querySelector("#texto_cifrado");
+let texto_encriptar = document.querySelector("#texto_encriptar");
 
 let btncopiar = document.querySelector("#copiar");
 
-btncopiar.addEventListener("click", (e) => {
+btncopiar.addEventListener("click", async (e) => {
   e.preventDefault();
+
   textoCifrado.select();
-  navigator.clipboard.writeText(textoCifrado.value.toLowerCase())
+  await navigator.clipboard.writeText(textoCifrado.value.toLowerCase())
+  const paste = await navigator.clipboard.readText()
+  texto_encriptar.value = paste;
 });
+
